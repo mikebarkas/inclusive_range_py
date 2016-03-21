2636ec8ebed232fe02761ddb1ddb948db703c4ad
Mike Barkas <mike@barkas.com>
2016-03-20 22:35:53 -0400

    Moving code to new line.

--- inclusive_range.py
+++ inclusive_range.py
@@ -5,17 +5,20 @@ class InclusiveRange:
 
     def __init__(self, *args):
         numargs = len(args)
-        if numargs < 1: raise TypeError('Requires at least one argument')
+        if numargs < 1:
+            raise TypeError('Requires at least one argument')
+
         elif numargs == 1:
             self.stop = args[0]
             self.start = 0
             self.step = 1
         elif numargs == 2:
             (self.start, self.stop) = args
-            self.step =1
+            self.step = 1
         elif numargs == 3:
             (self.start, self.stop, self.step) = args
-        else: raise TypeError('maximum 3 arguments')
+        else:
+            raise TypeError('maximum 3 arguments')
 
     def __iter__(self):
         i = self.start
@@ -28,4 +31,5 @@ def main():
     for i in InclusiveRange(30):
         print(i)
 
-if __name__ == "__main__": main()
+if __name__ == "__main__":
+    main()
